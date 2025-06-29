439e9a25683e38d3effac45bd26f5b82a41a8ae0
mudassirpress <mudassirali295786@gmail.com>
2025-06-29 23:02:14 +0500

    recover user account

--- models/user.js
+++ models/user.js
@@ -1,31 +1,44 @@
 const mongoose = require("mongoose");
 const userschema = mongoose.Schema({
-  fullname: { type: String, required: true, trim: true },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: (value) => {
-        const result = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return result.test(value);
-      },
-      message: "Please enter a valid email address",
-    }
-  },
-  state: { type: String, default: "" },
-  city: { type: String, default: "" },
-  locality: { type: String, default: "" },
-  password: { type: String, required: true },
-  
-  // 👇 Add this line
-  role: {
-    type: String,
-    enum: ['admin', 'user'],
-    required: true,
-  }
-});
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+    },
 
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        validate:{
+            validator:(value)=>{
+                const result =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return result.test(value);
+            },
+            massege : "Please enter a valid email address",
+        }
+    },
+
+    state:{
+        type:String,
+        default:"",
+    },
+
+    city:{
+        type:String,
+        default:"",
+    },
+
+    locality:{
+        type:String,
+        default:"",
+    },
+
+    password:{
+        type:String,
+        required:true,
+    },
+});
 
 const User = mongoose.model("User",userschema);
 
